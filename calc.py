fb31fd1d0cdf75ec275d5e55ad7e28f226e9c674
vadix <vadim.molchanov@gmail.com>
2015-08-24 11:46:23 +0300

    первое действие работает

--- calc.py
+++ calc.py
@@ -1,8 +1,14 @@
-import math.pow
+import math
 
-a = input(float())
-b = input(float())
-c = input(str())
+print("Ведите первое число: ")
+x = float(input())
+print("Ведите второе число: ")
+y = float(input())
+print("Ведите действие с числами: ")
+calc = input(str())
 
-if b == 0 and c = "mod":
-    print(Деление на 0!)
+
+if calc == "pow":
+    print(pow(x, y))
+else:
+    print(" ")
