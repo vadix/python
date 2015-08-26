ec70279ecb7b45773b1c60556b620a245e80b055
vadix <vadim.molchanov@gmail.com>
2015-08-26 12:15:10 +0300

    final

--- calc.py
+++ calc.py
@@ -1,34 +1,31 @@
 # coding=utf-8
 import math
 
-print("Ведите первое число: ")
-x = input()
-print("Ведите второе число: ")
-y = input()
-print("Ведите действие с числами: ")
+
+x = float(input())
+y = float(input())
 calc = input()
-if x == "" or y == "" or calc == "":
-    print()
-else:
-    float(x)
-    float(y)
-    assert isinstance(calc, object)
-    str(calc)
-    if calc == "pow":
-        print(pow(x, y))
-    elif calc == "div":
-        print(x // y)
-    elif calc == "+":
-        print(x + y)
-    elif calc == "-":
-        print(x - y)
-    elif calc == "/":
+
+if calc == "+":
+    print(x + y)
+if calc == "-":
+    print(x - y)
+if calc == "/":
+    if y != 0:
         print(x / y)
-    elif calc == "*":
-        print(x * y)
-    elif calc == "mod" and y == 0:
+    else:
         print("Деление на 0!")
-    elif calc == "mod":
+if calc == "*":
+    print(x * y)
+if calc == "mod":
+    if y != 0:
         print(x % y)
     else:
-        print()
+        print("Деление на 0!")
+if calc == "pow":
+    print(pow(x, y))
+if calc == "div":
+    if y != 0:
+        print(x // y)
+    else:
+        print("Деление на 0!")
