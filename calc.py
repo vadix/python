2952dd5b1b1dbd66b5aae8211768918e5555a4e1
vadix <vadim.molchanov@gmail.com>
2015-08-26 10:19:13 +0300

    1

--- calc.py
+++ calc.py
@@ -1,44 +1,34 @@
+# coding=utf-8
 import math
 
 print("Ведите первое число: ")
 x = input()
-if x =="":
-    exit
-else:
-    float(x)
-    
 print("Ведите второе число: ")
 y = input()
-if y =="":
-    exit
-else:
-    float(y)
-    
 print("Ведите действие с числами: ")
 calc = input()
-if calc =="":
-    exit
-else:
-    str(calc)
-
-
-if calc == "pow":
-    print(pow(x, y))
-elif calc == "div":
-    print(x // y)
-elif calc == "+":
-    print(x + y)
-elif calc == "-":
-    print(x - y)
-elif calc == "/":
-    print(x / y)
-elif calc == "*":
-    print(x * y)
-elif calc == "mod" and y == 0:
-    print("Деление на 0!")
-elif calc == "mod":
-    print(x % y)
-else:
+if x == "" or y == "" or calc == "":
     print()
-
-
+else:
+    float(x)
+    float(y)
+    assert isinstance(calc, object)
+    str(calc)
+    if calc == "pow":
+        print(pow(x, y))
+    elif calc == "div":
+        print(x // y)
+    elif calc == "+":
+        print(x + y)
+    elif calc == "-":
+        print(x - y)
+    elif calc == "/":
+        print(x / y)
+    elif calc == "*":
+        print(x * y)
+    elif calc == "mod" and y == 0:
+        print("Деление на 0!")
+    elif calc == "mod":
+        print(x % y)
+    else:
+        print()
