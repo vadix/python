1faa29cdb6f1f51c25b0d48d04e41b39918fc1b9
Vadim Molchanov <vadim.molchanov@gmail.com>
2017-04-23 21:50:03 +0300

    orginized structure added 4 chapter

---
+++ 4/4-13 food_table.py
@@ -0,0 +1,9 @@
+food = ('potato', 'salmon', 'meat', 'vegie' , 'vine')
+
+for dish in food:
+    print(dish)
+
+food = ('potato', 'fish', 'meat', 'carrot', 'vine')
+print('\n')
+for dish in food:
+    print(dish)
