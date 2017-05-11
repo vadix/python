57bf85d51496a9d16a6fdc4c183c8cba0e459706
vmolchanov <vmolchanov@kyriba.com>
2017-05-11 18:05:10 +0300

    added part of exercises form 3 chapter

--- 5/5-10 unic_name.py
+++ 5/5-10 unic_name.py
@@ -1,2 +1,8 @@
-current_users =[]
-new_users = []
+current_users =['admin', 'vadix', 'gleb', 'timo', 'nastya']
+new_users = ['tanya', 'Admin', 'Stas', 'Vadix', 'lena']
+
+for current_user in current_users:
+    if current_user.title() in new_users:
+        print(current_user + " take new name")
+    else:
+        print(current_user + " ok")
