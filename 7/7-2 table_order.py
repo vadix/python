645584e2e626dcfe09511366951a09a765d91bcb
vmolchanov <vmolchanov@kyriba.com>
2017-06-12 18:58:00 +0300

    7 chapter

---
+++ 7/7-2 table_order.py
@@ -0,0 +1,6 @@
+message = input("How many guests? ")
+message = int(message)
+if message < 8:
+    print("Yes, we have a table")
+else:
+    print("Please, wait")
---
+++ 7/7-3 number_10.py
@@ -0,0 +1,6 @@
+number = input("Enter any number ")
+number = int(number)
+if number % 10 == 0:
+    print('it\'s divided by ten' )
+else:
+    print('it\'s not divided by ten' )
