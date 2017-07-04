65df41d81770d38772c457c66f18444c19c63ad9
vmolchanov <vmolchanov@kyriba.com>
2017-07-04 16:40:16 +0300

    8 chapter

---
+++ 8/8-3 make_shirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size, text):
+        """ print size and text"""
+        print("Your size is " + size + ", with label " + text)
+
+make_shirt("XL", "hello!")
---
+++ 8/8-4 make_big_shirt.py
@@ -0,0 +1,7 @@
+def make_shirt("L", "I love Python"):
+    """ print size and text"""
+    print("Your size is " + size + ", with label " + text)
+
+
+make_shirt("XL", "hello!")
+make_shirt()
