d1f5c3cedfba69bf18c3b446e33078f08a385704
vadim molchanov <vadim.molchanov@gmail.com>
2017-08-06 23:32:49 +0300

    no c m

---
+++ 8/8-9 magicians.py
@@ -0,0 +1,8 @@
+def show_magicians(names):
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+
+magicians = ['lsjdfs', 'lsjdf', 'slkjfsjf']
+
+show_magicians(magicians)
