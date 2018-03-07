36bf8da6f9329442a98a43332be04e25181fc7eb
vmolchanov <vmolchanov@kyriba.com>
2018-03-07 16:34:37 +0300

    ttt

--- 11/city_functions.py
+++ 11/city_functions.py
@@ -1,6 +1,7 @@
 def get_formatted_city(city, country, population=''):
     """Строит отформатированную строку Santiago, Chile."""
-    if population = ''
-        
-    formatted_city = city + ', ' + country + ' — population ' + population
+    if population:
+        formatted_city = city + ', ' + country + ' — population ' + population
+    else:
+        formatted_city = city + ', ' + country
     return formatted_city.title()
